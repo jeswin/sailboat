965db1f6e6d8914ba27b6142b857528a28dabd02
Jeswin Kumar <jeswinpk@agilehead.com>
2017-08-07 10:19:21 +0530

    WIP: NSOAP React

--- src/nsoap-react.js
+++ src/nsoap-react.js
@@ -1,20 +1,99 @@
-import core from "nsoap";
+import nsoap, { RoutingError } from "nsoap";
+
+const identifierRegex = /^[a-zA-Z_$][a-zA-Z_$0-9]*$/;
+
+function parseDict(dict) {
+  return key => {
+    if (Object.prototype.hasOwnProperty.call(dict, key)) {
+      const val = dict[key];
+      return {
+        value:
+          typeof val !== "string"
+            ? val
+            : val === "true" || val === "false"
+              ? val === "true"
+              : identifierRegex.test(val) ? `${val}` : JSON.parse(val)
+      };
+    }
+  };
+}
+
+function parseHeaders(headers) {
+  return parseDict(headers);
+}
+
+function parseQuery(query) {
+  return parseDict(query);
+}
+
+function parseBody(body) {
+  return body || {};
+}
+
+function parseCookies(cookies) {
+  return parseDict(cookies);
+}
 
 export default function(app, options = {}) {
-  const urlPrefix = options.urlPrefix || "/";
-  return (req, res, next) => {
-    const body = options.body ? options.body(req) : req.body;
-    const { path, url, query } = req;
+  const _urlPrefix = options.urlPrefix || "/";
+  const urlPrefix = _urlPrefix.endsWith("/") ? _urlPrefix : `${urlPrefix}/`;
+
+  return (req, res) => {
+    const { path, url, query, headers } = req;
+
     if (path.startsWith(urlPrefix)) {
+      const body = options.body ? options.getBody(req) : req.body;
+      const cookies = options.getCookies
+        ? options.getCookies(req)
+        : req.cookies;
+
       const strippedPath = path.substring(urlPrefix.length);
       const dicts = [
-        options.parseHeaders ? options.parseHeaders(headers) : headers,
-        options.parseQuery ? options.parseQuery(query) : query,
-        options.parseBody ? options.parseBody(body) : body
+        options.parseHeaders
+          ? options.parseHeaders(headers)
+          : parseHeaders(headers),
+        options.parseQuery ? options.parseQuery(query) : parseQuery(query),
+        options.parseBody ? options.parseBody(body) : parseBody(body),
+        options.parseCookies
+          ? options.parseCookies(cookies)
+          : parseCookies(cookies)
       ];
-      nsoap(app, strippedPath, dicts, { index: options.index }, () => next());
-    } else {
-      next();
+
+      const createContext = options.createContext || (x => x);
+      const context = options.appendContext
+        ? createContext({ req, res, isContext: () => true })
+        : [];
+
+      nsoap(app, strippedPath, dicts, {
+        index: options.index || "index",
+        prependArgs: options.contextAsFirstArgument,
+        args: [context]
+      }).then(
+        result => {
+          if (result instanceof RoutingError) {
+            if (result.type === "NOT_FOUND") {
+              res.status(404).send("Not found.");
+            } else {
+              res.status(500).send("Server error");
+            }
+          } else if (typeof result === "function") {
+            result.apply(undefined, [req, res]);
+          } else {
+            if (!context.handled) {
+              if (typeof result === "string" && !options.alwaysUseJSON) {
+                res.status(200).send(result);
+              } else {
+                res.status(200).json(result);
+              }
+            }
+          }
+        },
+        error => {
+          if (!context.handled) {
+            res.status(400).send(error);
+          }
+        }
+      );
     }
   };
 }
--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,297 @@
+import nsoap from "../nsoap-express";
+import should from "should";
+import express from "express";
+import request from "supertest";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
+const Template = ({ title, content, children }) =>
+  <div>
+    <header>
+      {title}
+    </header>
+    <p>
+      {content}
+    </p>
+    {React.Children.count(children) ? children : <div />}
+  </div>;
 
-class Parent extends Component {
-  render() {
+const HomePage = props =>
+  <Template title="Home Page" content="Welcome to NSOAP React" />;
 
+const ParentPage = ({ parentId, children }) =>
+  <div>
+    <h1>
+      Parent id: ${parentId}
+    </h1>
+    <div>
+      {children}
+    </div>
+  </div>;
+
+const ChildComponent = ({ childId, children }) =>
+  <div>
+    <h2>
+      Child id: ${childId}
+    </h2>
+    <div>
+      {children}
+    </div>
+  </div>;
+
+const GrandChildComponent = ({ grandChildId }) =>
+  <div>
+    GrandChild {grandChildId}
+  </div>;
+
+const routes = {
+  index() {
+    return <HomePage />;
+  },
+  parent(parentId) {
+    return defineParent(ParentPage, { parentId }, Component => ({
+      index: Component(),
+      child(childId) {
+        return defineComponent(
+          Component,
+          ChildComponent,
+          { childId },
+          Component => ({
+            index: Component(),
+            grandChild1(grandChildId) {
+              return Component(
+                <GrandChildComponent grandChildId={grandChildId} />
+              );
+            }
+          })
+        );
+      }
+    }));
   }
-}
+};
 
-const app = {
-  index: sessionId => ({
-    index: <Parent />
-  })
-}
+const routesAlt = {
+  index: [HomePage],
+  parent: [
+    ParentPage,
+    parentId => ({ parentId }),
+    {
+      child: [
+        ChildComponent,
+        childId => ({ childId }),
+        {
+          grandChild1: [
+            GrandChildComponent,
+            () => ({
+              id: "1"
+            })
+          ],
+          grandChild2: [
+            GrandChildComponent,
+            () => ({
+              id: "2"
+            })
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+describe("NSOAP Express", () => {
+  it("Calls a parameter-less function", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/about");
+    resp.text.should.equal("NSOAP Test Suite");
+  });
+
+  it("Gets the value of a property", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/static");
+    resp.text.should.equal("NSOAP Static File");
+  });
+
+  it("Calls a unary function", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/unary(10)");
+    resp.body.should.equal(20);
+  });
+
+  it("Throws an exception", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/throw(10)");
+    resp.status.should.equal(400);
+    resp.error.should.not.be.empty();
+  });
+
+  it("Calls a binary function", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/binary(10,20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Calls a unary function with variables", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/unary(x)?x=20");
+    resp.body.should.equal(30);
+  });
+
+  it("Calls a binary function with variables", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/binary(x,y)?x=10&y=20");
+    resp.body.should.equal(30);
+  });
+
+  it("Calls a binary function with literals and variables", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/binary(x,20)?x=10");
+    resp.body.should.equal(30);
+  });
+
+  it("Calls a binary function in a namespace", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/namespace.binary(10,20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Calls a binary function in a nested namespace", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/nested.namespace.binary(10,20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Accepts stringified JSON arguments in querystring", async () => {
+    const app = makeApp();
+    const obj = encodeURIComponent(JSON.stringify({ x: 10 }));
+    const resp = await request(app).get(`/json(obj)?obj=${obj}`);
+    resp.body.should.equal(30);
+  });
+
+  it("Accepts JSON arguments in body", async () => {
+    const app = makeApp();
+    const resp = await request(app).post("/json(obj)").send({ obj: { x: 10 } });
+    resp.body.should.equal(30);
+  });
+
+  it("Accepts arguments in headers", async () => {
+    const app = makeApp();
+    const resp = await request(app)
+      .post("/binary(x,y)")
+      .set("x", 10)
+      .set("y", 20);
+    resp.body.should.equal(30);
+  });
+
+  it("Accepts arguments in cookies", async () => {
+    const app = makeApp();
+    const resp = await request(app)
+      .post("/binary(x,y)")
+      .set("Cookie", ["x=10", "y=20"]);
+    resp.body.should.equal(30);
+  });
+
+  it("Obeys parameter precedence (header, query, body, cookies)", async () => {
+    const app = makeApp();
+    const resp = await request(app)
+      .post("/tripletAdder(x,y,z)?x=2&y=20")
+      .set("x", 1)
+      .set("Cookie", ["x=4", "y=40", "z=400"])
+      .send({ x: 3, y: 30, z: 300 });
+    resp.body.should.equal(321);
+  });
+
+  it("Adds parenthesis if omitted", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/about");
+    resp.text.should.equal("NSOAP Test Suite");
+  });
+
+  it("Calls the default function", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/");
+    resp.text.should.equal("Home page!");
+  });
+
+  it("Calls chained functions", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/chainAdder1(10).chainAdder2(20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Infers types", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/infer(true,20,Hello)");
+    resp.body._bool.should.equal(true);
+    resp.body._num.should.equal(20);
+    resp.body._str.should.equal("Hello");
+  });
+
+  it("Is Case-sensitive", async () => {
+    const app = makeApp();
+    const resp = await request(app)
+      .post("/json(obj)")
+      .send({ obj: { X: 100, x: 10 } });
+    resp.body.should.equal(30);
+  });
+
+  it("Resolves a Promise", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/promiseToAdd(10,20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Calls a function on the resolved value of a Promise", async () => {
+    const app = makeApp();
+    const resp = await request(app).get(
+      "/functionOnPromise(x,y).adder(100)?x=10&y=20"
+    );
+    resp.body.should.equal(130);
+  });
+
+  it("Calls default function on object", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/defaultFunction(10,20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Passes context as an argument", async () => {
+    const app = makeApp({ appendContext: true });
+    const resp = await request(app).get("/funcWithContext(10,20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Passes context as the first argument", async () => {
+    const app = makeApp({ appendContext: true, contextAsFirstArgument: true });
+    const resp = await request(app).get("/funcWithPrependedContext(10,20)");
+    resp.body.should.equal(30);
+  });
+
+  it("Overrides request handling", async () => {
+    const app = makeApp({ appendContext: true, contextAsFirstArgument: true });
+    const resp = await request(app).get("/overrideResponse(10,20)");
+    resp.text.should.equal("200");
+  });
+
+  it("Passes a custom context", async () => {
+    const app = makeApp({
+      appendContext: true,
+      contextAsFirstArgument: true,
+      createContext: args => ({ ...args, z: 10 })
+    });
+    const resp = await request(app).get("/customContext(10,20)");
+    resp.body.should.equal(40);
+  });
+
+  it("Calls a raw handler", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/rawHandler(10,20)");
+    resp.text.should.equal("200");
+  });
+
+  it("Returns 404 if not found", async () => {
+    const app = makeApp();
+    const resp = await request(app).get("/nonExistantFunction(10,20)");
+    resp.status.should.equal(404);
+    resp.text.should.equal("Not found.");
+  });
+});
